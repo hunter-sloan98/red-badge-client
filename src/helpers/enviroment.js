73d704e482056b740a12142ee66a91e1f6e7f11e
hunter-sloan98 <sloan.hunter98@gmail.com>
2021-10-07 07:44:59 -0400

    APIURL again

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -1,12 +1,12 @@
 let APIURL = '';
-
+console.log(window.location.hostname)
 switch(window.location.hostname) {
   case 'localhost' || '127.0.0.1': 
     APIURL = 'http://localhost:3005';
     break;
 
-  case 'red-badge-project.herokuapp.com':
-    APIURL = 'https://red-badge-project-client.herokuapp.com';
+  case 'red-badge-project-client.herokuapp.com':
+    APIURL = 'https://red-badge-project.herokuapp.com';
     break;
 }
 
